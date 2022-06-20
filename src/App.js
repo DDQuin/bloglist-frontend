43587e8ba69936e1c842bb22441d64fa92901e9a
DDQuin <ddquin420@gmail.com>
2022-06-20 13:57:00 +0100

    ex 5.2

--- src/App.js
+++ src/App.js
@@ -16,6 +16,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -23,6 +32,11 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -32,6 +46,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const logoutUser = async (event) => {
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogappUser')
   }
 
   const loginForm = () => (
@@ -63,7 +82,7 @@ const App = () => {
       <div>
       <h2>blogs</h2>
 
-      <p>{user.name} logged-in</p>
+      <p>{user.name} logged-in <button onClick={logoutUser}>logout</button></p>
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />

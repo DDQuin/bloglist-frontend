d097368bbb5ff49bdb240a73dd08016c63ef4aba
DDQuin <ddquin420@gmail.com>
2022-06-21 11:21:28 +0100

    ex 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({blog}) => {
+  const [displayed, setDisplay] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  if (!displayed) {
+    return (
+      <div style={blogStyle}>
+      <div>
+        {blog.title} <button onClick={() => setDisplay(true)}>view</button>
+      </div>
+      </div>  
+      )
+  } else {
+    return (
+      <div style={blogStyle}>
+      <div>
+        {blog.title}  <button onClick={() => setDisplay(false)}>hide</button>
+        <br></br>
+        {blog.url}
+        <br></br>
+        likes {blog.likes} <button>like</button>
+        <br></br>
+        {blog.author}
+      </div>
+      </div>  
+      )
+  }
+  
+}
 
 export default Blog

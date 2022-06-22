272d1d1eb3774b3398ad5a4d440486d51f09e734
DDQuin <ddquin420@gmail.com>
2022-06-22 20:10:23 +0100

    ex 5.18

--- cypress/e2e/blog_spec.cy.js
+++ cypress/e2e/blog_spec.cy.js
@@ -1,10 +1,40 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'AdminUser',
+      username: 'admin',
+      password: 'root'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user) 
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('admin')
+    cy.get('#password').type('root')
+    cy.get('#login-button').click()
+    cy.get('html').should('contain', 'AdminUser logged-in')
+    })
+
+    it('fails with wrong credentials', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('admin')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+    .should('contain', 'Wrong credentials')
+    .and('have.css', 'color', 'rgb(255, 0, 0)')
+    .and('have.css', 'border-style', 'solid')
+
+    cy.get('html').should('not.contain', 'AdminUser logged in')
+    })
   })
 })
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -26,6 +26,7 @@ const LoginForm = ({ login }) => {
           value={username}
           name="Username"
           onChange={({ target }) => setUsername(target.value)}
+          id='username'
         />
       </div>
       <div>
@@ -35,9 +36,10 @@ const LoginForm = ({ login }) => {
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
+          id='password'
         />
       </div>
-      <button type="submit">login</button>
+      <button type="submit" id='login-button'>login</button>
     </form>
   )
 }
